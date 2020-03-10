942d6689dfb25beaf0f8bb99d5083dffe342f74b
Larry Shumlich <lshumlich@gmail.com>
2020-03-10 14:06:18 -0600

    Check our the insertAdjecent

--- dompres/src/dom.js
+++ dompres/src/dom.js
@@ -1,14 +1,20 @@
 
 console.log(idButton);
 
-idButton.addEventListener("click", e => {
+document.body.addEventListener("click", e => {
     console.log("You Clicked, at your command");
+    console.log(e.target.textContent);
+    console.log(e.target.nodeName);
+    console.log(e.target);
 
-    const div = document.createElement("div");
-    div.className = "clCard";
-    div.appendChild(document.createTextNode("This has been inserted"));
+    
+    
 
-    idStuff2.insertAdjacentElement("afterend", div);
+    // const div = document.createElement("div");
+    // div.className = "clCard";
+    // div.appendChild(document.createTextNode("This has been inserted"));
+
+    // idStuff2.insertAdjacentElement("afterend", div);
 
 
 })
