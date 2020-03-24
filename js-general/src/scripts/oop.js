5c6120abe6d45a337dd6a94dfd96a669e6d4cafc
Larry Shumlich <lshumlich@gmail.com>
2020-03-24 13:42:38 -0600

    Start on TDD wiht OO

---
+++ js-general/src/scripts/oop.js
@@ -0,0 +1,8 @@
+
+const oopFunctions = {
+    play() {
+        return "You have run play...";
+    }
+}
+
+export default oopFunctions;
---
+++ js-general/src/scripts/oop.test.js
@@ -0,0 +1,10 @@
+//
+// npm test -- -t whatever
+//
+import oofuncs from './oop.js';
+
+test('Test the plumbing', () => {
+    console.log("Test the unit test plumbing");
+    console.log(oofuncs.play());
+    
+});
