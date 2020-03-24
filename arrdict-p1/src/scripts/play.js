15ffe6fd7d64344897694e5c57ca3ff6079ed21c
Larry Shumlich <lshumlich@gmail.com>
2020-03-24 10:37:19 -0600

    work with dictionaries and arrays

---
+++ arrdict-p1/src/scripts/play.js
@@ -0,0 +1,15 @@
+/*
+    Author: Larry Shumlich
+    Date:   March 24, 2020
+    Abstract:
+        This code is to demonstrate how to use unit tests to discover 
+        just how cool JavaScript really is.
+
+*/
+const playFunctions = {
+    play(val) {
+        return [val]
+    },
+}
+
+export default playFunctions;
---
+++ arrdict-p1/src/scripts/play.test.js
@@ -0,0 +1,61 @@
+//
+// npm test -- -t whatever
+//
+import pfunc from './play.js';
+
+test('Test the plumbing', () => {
+    console.log("Test the unit test plumbing");
+    expect(123).toBe(123);
+});
+
+test('Test to learn how arrays work.', () => {
+    const arr1 = [5, 6, 7];
+    expect(arr1.length).toBe(3);
+    expect(arr1[1]).toBe(6);
+
+    arr1.push(12);
+    expect(arr1.length).toBe(4);
+    expect(arr1[3]).toBe(12);
+
+    arr1.unshift(15);
+    expect(arr1.length).toBe(5);
+    expect(arr1[0]).toBe(15);
+
+    // for (let i = 0; i < arr1.length; i++) {
+    //     console.log(i, arr1[i]);
+    // }
+
+    arr1.forEach( (val, i, ar) => {
+        // console.log(val, i, ar);
+    });
+
+    // console.log(arr1);
+    
+    // console.log("Test the unit test plumbing");
+    expect(123).toBe(123);
+});
+
+
+test('Test to learn how dictionaries work.', () => {
+
+    const obj = {ab:'Alberta', sk:'Saskatchewan'};
+    console.log(obj['ab']);
+    obj['mn'] = "Manatoba";
+    console.log(obj);
+
+    for (let v in obj) {
+        console.log(v, obj[v]);
+        
+    }
+});
+
+
+test('Test the link to play.js.', () => {
+    console.log('In link test');
+    const retval = pfunc.play(123);
+    expect(retval[0]).toBe(123);
+    
+    expect(pfunc.play(234)[0]).toBe(234);
+
+
+});
