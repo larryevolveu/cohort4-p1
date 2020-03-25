9a80fdb4784a5003450ed16ff668c5ffd7d6cc91
Larry Shumlich <lshumlich@gmail.com>
2020-03-25 14:00:20 -0600

    Add a second DOM example

---
+++ dompres/src/dom-p2.test.js
@@ -0,0 +1,57 @@
+// import domfuncs from './domfunc'
+
+test('Does the dom functions work?', () => {
+    const parent = document.createElement('div');
+
+    let div = document.createElement('div');
+    let txtstuff = document.createTextNode("First");
+    div.appendChild(txtstuff);
+    parent.append(div);
+
+    div = document.createElement('div');
+    txtstuff = document.createTextNode("Second");
+    div.appendChild(txtstuff);
+    parent.append(div);
+
+    div = document.createElement('div');
+    txtstuff = document.createTextNode("One.One");
+    div.appendChild(txtstuff);
+
+    parent.children[0].after(div);
+
+
+
+    // div.appendChild(txtstuff);
+
+
+
+
+    console.log(parent.children.length);
+    console.log(parent.children[0]);
+    console.log(parent.children[0].textContent);
+    console.log(parent.children[1]);
+    console.log(parent.children[1].textContent);
+    console.log(parent.children[2]);
+    console.log(parent.children[2].textContent);
+
+    document.body.append(div);
+    
+    
+    // console.log(div.textContent);
+    // console.log(div);
+    // console.log(div);
+    
+});
+
+
+
+
+// const element = domfuncs.buildCard();
+// expect(element).toBeTruthy();
+
+    // expect(functions.add(1,2)).toBe(3);
+    // expect(functions.add(101,202)).toBe(303);
+
+
+// const div = document.createElement('div');
+// div.appendChild(document.createTextNode(text));
--- dompres/src/domfunc.test.js
+++ dompres/src/domfunc.test.js
@@ -29,3 +29,28 @@ test('Does the addBefore functions work?', () => {
     expect(group.children[1].textContent.substr(0,12)).toBe("First Insert");
 
 });
+
+
+test('Does the after functions work?', () => {
+    // expect(functions.add(1,2)).toBe(3);
+    // expect(functions.add(101,202)).toBe(303);
+    const group = document.createElement("div");
+    const element = domfuncs.buildCard("First Insert");
+    group.appendChild(element);
+
+    const second = domfuncs.buildCard("Second");
+
+   element.after(second);
+    // group.appendChild(second);
+
+    let node = domfuncs.buildCard("one.one");
+
+    second.before(node);
+
+    console.log(group);
+    console.log(group.children[0].textContent);
+    console.log(group.children[1].textContent);
+    console.log(group.children[2].textContent);
+
+});
+
